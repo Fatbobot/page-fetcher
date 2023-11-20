35dd03f023d07d677d0843fd22dec2e88fc7e629
Paul Jean <Fatbobot@gmail.com>
2023-11-20 18:58:19 -0400

    lint some code

--- fetcher.js
+++ fetcher.js
@@ -1,13 +1,13 @@
+//reads input from terminal, store in variables
 let args = process.argv.slice(2);
+let webpage = args[0];
+let whereTo = args[1];
 // requiring stuff
 const fs = require("fs");
 const request = require("request");
-// const net = require("net");
 
-let webpage = args[0];
-let whereTo = args[1];
 
-// writing file function
+// callback for request function. Writes file after connecting to website
 const writeFile = function (whereTo, webpage) {
   fs.writeFile(whereTo, webpage, (err) => {
     if (err) {
@@ -15,10 +15,10 @@ const writeFile = function (whereTo, webpage) {
     }
   });
 };
-
+// runs when calling fetcher.js in terminal. stores webpage data in body
 request(webpage, (error, response, body) => {
   console.log("error:", error); // Print the error if one occurred
   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
   writeFile(whereTo, body);
-  console.log('Downloaded and saved ${'body.length}, to ${'whereTo'})
+  console.log(`Downloaded and saved ${body.length} bytes to ${whereTo}`);
 });
